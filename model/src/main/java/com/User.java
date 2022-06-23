c637eea0ebc45bb3de9dbcaea53210b8db2f9f76
Dan Nguyen <danhieunguyen@kms-technology.com>
2022-06-23 17:02:49 +0700

    fix: Get Users

--- model/src/main/java/com/User.java
+++ model/src/main/java/com/User.java
@@ -13,13 +13,13 @@ import java.time.Period;
 public class User {
     @Id
     @SequenceGenerator(
-            name = "student_sequence",
-            sequenceName = "student_sequence",
+            name = "user_sequence",
+            sequenceName = "user_sequence",
             allocationSize = 1
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
-            generator = "student_sequence"
+            generator = "user_sequence"
     )
     @Column(name = "id")
     private Long id;
--- service/src/main/java/com/UserService.java
+++ service/src/main/java/com/UserService.java
@@ -2,8 +2,11 @@ package com;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface UserService{
     User findUserByEmail(String email) throws UserNotFoundException;
     void AddNewUser(User user);
+    List<User> getAllUsers();
 }
