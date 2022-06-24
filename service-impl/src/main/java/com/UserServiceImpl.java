9c13b958eae038c2796323a995f7d0c014b19c5c
Dan Nguyen <danhieunguyen@kms-technology.com>
2022-06-24 16:19:41 +0700

    feat: logging service

--- service-impl/src/main/java/com/UserServiceImpl.java
+++ service-impl/src/main/java/com/UserServiceImpl.java
@@ -1,12 +1,13 @@
 package com;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
-
+@Slf4j
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService{
@@ -17,9 +18,12 @@ public class UserServiceImpl implements UserService{
         Optional<User> optionalUser = userRepository.findByEmail(email);
 
         if (optionalUser.isEmpty())
+        {
+            log.error(String.format("No such user with %s: %s","email",email));
             throw new UserNotFoundException("email",email);
-        else
-            return optionalUser.get();
+        }
+        log.info(String.format("Founded user with %s: %s","email",email));
+        return optionalUser.get();
 
     }
     @Override
@@ -27,9 +31,13 @@ public class UserServiceImpl implements UserService{
         User user = userMapper.INSTANCE.toEntity(userDTO);
         Optional<User> userExist = userRepository.findByEmail(userDTO.getEmail());
         if(userExist.isPresent())
+        {
+            log.error(String.format("Email already taken when use: %s",userDTO.getEmail()));
             throw new IllegalStateException("Email already taken!");
-        else
-            userRepository.save(user);
+        }
+
+        userRepository.save(user);
+        log.info(String.format("Created user with email: %s",userDTO.getEmail()));
 
     }
 
@@ -37,8 +45,11 @@ public class UserServiceImpl implements UserService{
     public List<User> getAllUsers() {
         List<User> userList = userRepository.findAll();
         if(userList.isEmpty())
+        {
+            log.error("Not found any users");
             throw new IllegalStateException("Not found any users");
-
+        }
+        log.info(String.format("Get %d users",userList.stream().count()));
         return userList;
     }
 
@@ -47,10 +58,12 @@ public class UserServiceImpl implements UserService{
         User user = findUserByEmail(email);
         userMapper.INSTANCE.updateEntity(userDTO,user);
         userRepository.save(user);
+        log.info(String.format("Update user with email: %s ",email));
     }
     @Override
     public void deleteUser(String email){
         User user = findUserByEmail(email);
         userRepository.delete(user);
+        log.info(String.format("Deleted user with email: %s ",email));
     }
 }
